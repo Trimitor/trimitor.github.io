4297cdba86cfebca0609877464a98d806c49c011
Trimitor <73941033+Trimitor@users.noreply.github.com>
2025-05-31 22:19:08 +0300

    upd

--- pages/about.js
+++
@@ -1,7 +0,0 @@
-export default async function({ template }) {
-  return Mustache.render(template, { title: "About Page", message: "This is the about page." });
-}
-
-export function after({ params, query }) {
-  console.log("After render for /about", { params, query });
-}
