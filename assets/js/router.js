86542b9007b0c2df7cb17a111084ba8d7f8ee2fa
Trimitor <73941033+Trimitor@users.noreply.github.com>
2025-05-31 16:33:46 +0300

    Upd

--- assets/js/router.js
+++ assets/js/router.js
@@ -19,17 +19,39 @@ async function fetchTemplate(name) {
   return await res.text();
 }
 
-async function loadView(viewName) {
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+async function loadView(viewName, params = {}, query = {}) {
   try {
     const module = await import(`../../pages/${viewName}.js`);
+    const handler = module.default || module[`load${capitalize(viewName)}`] || module.load;
+
     const template = await fetchTemplate(viewName);
-    const html = await module.default({ template });
-    document.querySelector('.content').innerHTML = html;
+    const html = await handler({ params, query, template });
+
+    const container = document.querySelector('.content');
+    if (container) {
+      container.classList.remove('show');
+
+      setTimeout(() => {
+        container.innerHTML = html;
+        container.classList.add('show');
+
+        if (typeof module.after === 'function') {
+          module.after({ params, query });
+        }
+      }, 150);
+    }
   } catch (err) {
+    console.error(err);
     const template = await fetchTemplate('404');
-    document.querySelector('.content').innerHTML = Mustache.render(template, {});
+    const html = Mustache.render(template, {});
+    document.querySelector('.content').innerHTML = html;
   }
 }
+
 
 export async function navigate(path) {
   const match = matchRoute(path);
--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,7 @@
 export default async function({ template }) {
   return Mustache.render(template, { title: "About Page", message: "This is the about page." });
 }
+
+export function after({ params, query }) {
+  console.log("After render for /about", { params, query });
+}
