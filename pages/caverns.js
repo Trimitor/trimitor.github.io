8a36728285862709eecb2ca0ed83143ad94b66e4
Trimitor <73941033+Trimitor@users.noreply.github.com>
2025-06-01 03:33:31 +0300

    upd

--- pages/caverns.js
+++ pages/caverns.js
@@ -2,3 +2,9 @@ export default async function ({ template }) {
     document.title = 'Caverns and Mines Data | Trimitor';
     return Mustache.render(template, {});
 }
+
+export async function after() {
+    let caverns = await fetch('data/caverns.json');
+    caverns = await caverns.json();
+    console.log(caverns);
+}
